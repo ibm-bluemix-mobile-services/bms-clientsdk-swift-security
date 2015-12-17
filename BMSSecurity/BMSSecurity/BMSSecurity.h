6d197d400501ddfd88036e7a61691f89687ad18e
vitalym <vitalym@il.ibm.com>
2015-12-17 16:22:27 +0200

    Create empty BMSSecurity Swift project

---
+++ BMSSecurity/BMSSecurity/BMSSecurity.h
@@ -0,0 +1,19 @@
+//
+//  BMSSecurity.h
+//  BMSSecurity
+//
+//  Created by Vitaly Meytin on 12/17/15.
+//  Copyright © 2015 IBM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMSSecurity.
+FOUNDATION_EXPORT double BMSSecurityVersionNumber;
+
+//! Project version string for BMSSecurity.
+FOUNDATION_EXPORT const unsigned char BMSSecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMSSecurity/PublicHeader.h>
+
+
